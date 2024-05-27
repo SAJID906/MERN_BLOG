6b2cb771f36706a2079bc69d78e6c865c1344df7
SAJID906 <sajidue906@gmail.com>
2024-05-27 11:31:44 +0500

    Creates Pages and Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 function App() {
-  return <div className="bg-yellow-200 text-center text-red-700">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/sing-in" element={<Signin />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
